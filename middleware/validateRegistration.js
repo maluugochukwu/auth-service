6f05b54e7a3e0d3fa0c16e9c395ddf47233939bd
Ugochukwu <maluugochukwu@gmail.com>
2022-04-29 22:13:00 +0100

    Refactored registration validation

--- middleware/validateRegistration.js
+++ middleware/validateRegistration.js
@@ -1,9 +1,7 @@
-const { body, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
+
+
 const registerValidation = (req,res,next)=>{
-    // username must be an email
-    body('username').isEmail()
-    // password must be at least 5 chars long
-    body('password').isLength({ min: 5 })
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
        res.status(400).json({ errors: errors.array() });
--- routes/register.js
+++ routes/register.js
@@ -1,8 +1,18 @@
 const app      = require('express');
 const register = app.Router();
 const validateRegistration = require("../middleware/validateRegistration");
+const { body } = require('express-validator');
 
-register.post('/',validateRegistration,(req,res)=>{
+const schemaRule = [
+    body("username").isEmail()
+]
+    
+    
+
+
+register.post('/',schemaRule,validateRegistration,
+(req,res)=>{
+    
     res.send("You have reached the register route.");
 })
 module.exports = register;
