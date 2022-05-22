b4c9e527022151e2236f47b3bac5672c590941b8
ugochukwu <maluugochukwu@gmail.com>
2022-05-22 18:24:12 +0100

    google

--- controller/auth.js
+++ controller/auth.js
@@ -1,20 +1,42 @@
-const { models: {UserRefreshToken} }  = require('../model');
+const { models: {UserRefreshToken,User,UserRole} }  = require('../model');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-const login                 = async (req,res)=>{}
-const loginWithProvider     = async (req,res)=>{}
-const register              = async (req,res)=>{}
+const login = async (req,res)=>{
+    const {username} = req.body;
+    const tokens = await issueToken(username)
+    // console.log(tokens,"my tokens")
+    //save refreshToken to db for the user
+    UserRefreshToken.create({username: username,refresh_token:tokens.refreshToken});
+    
+    res.cookie('jwt',tokens.refreshToken, {httpOnly:true,maxAge:24 * 60 * 60 * 1000,samesite:'None',secure:true});
+
+    res.json({message:"login ok",accessToken:tokens.accessToken})
+}
+const loginWithProvider     = async (req,res)=>{
+    // check if user is already in database. if so get the user provider id
+    // if the provider id matches the one passed in the query parameters; then issue a token.
+    // if it does not match the one passed in the query parameters, notify the user to use the appropriate provider
+    // in the case where the user does not exist in the database, create a new user and issue a token
+}
+const register              = async (req,res)=>{
+
+}
 const registerWithProvider  = async (req,res)=>{}
-const issueToken            = async (req,res)=>{}
 
 const auth = async (req,res)=>{
-    const {username} = req.body;
     
+}
+const issueToken = async username =>{
+    const roles = await UserRole.findAll({where: {username: username}})
+   
+    const userRoles = []
+    roles.forEach(role => userRoles.push(role.role_id))
+    console.log(userRoles,"my roles")
     const accessToken = jwt.sign(
         {
             username:username,
-            role:[]
+            role:userRoles
         },
         process.env.ACCESS_TOKEN_SECRET,
         {expiresIn:'60s'}
@@ -22,17 +44,14 @@ const auth = async (req,res)=>{
     const refreshToken = jwt.sign(
         {
             username:username,
-            role:[]
+            role:userRoles
         },
         process.env.REFRESH_TOKEN_SECRET,
         {expiresIn:'1d'}
     );
-    //save refreshToken to db for the user
-    UserRefreshToken.create({username: username,refresh_token:refreshToken});
-    
-    res.cookie('jwt',refreshToken, {httpOnly:true,maxAge:24 * 60 * 60 * 1000,samesite:'None',secure:true});
-
-    res.json({message:"login ok",accessToken})
+    return {accessToken,refreshToken}
 }
 
-module.exports = auth;
+module.exports = {
+    login
+};
--- routes/auth.js
+++ routes/auth.js
@@ -1,21 +1,22 @@
 const app      = require('express');
-const auth = app.Router();
+const router = app.Router();
 const authMiddleware = require('../middleware/validateAuth')
 const authProviderMiddleware = require('../middleware/validateAuthProvider')
 const authRegistration = require('../middleware/validateRegistration')
+const auth = require('../controller/auth');
 
 // login with username and password
-auth.post('/',authMiddleware,require('../controller/auth'))
+router.post('/',authMiddleware,auth.login)
 
-// login with 3rd party auth provider
-auth.post('/:provider',authProviderMiddleware,require('../controller/auth'))
+// // login with 3rd party auth provider
+// router.post('/:provider',authProviderMiddleware,require('../controller/auth'))
 
-// register with username, password, and other details
-auth.post('/register',authRegistration,require('../controller/auth'))
+// // register with username, password, and other details
+// router.post('/register',authRegistration,require('../controller/auth'))
 
-// register with 3rd party auth provider
-auth.post('/register/:provider',authProviderMiddleware,require('../controller/auth'))
+// // register with 3rd party auth provider
+// router.post('/register/:provider',authProviderMiddleware,require('../controller/auth'))
 
 
 
-module.exports = auth;
+module.exports = router;
